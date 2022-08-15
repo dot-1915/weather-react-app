bcc8af9aae52087b9e36b28de515ea4ab1b6cf11
dot-1915 <dea.dorothy@gmail.com>
2022-08-15 09:48:47 -0700

    imported project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherSearch from "./WeatherSearch";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ fontSize: 50 }}>Weather Search</h1>
+    </div>
+  );
+}
